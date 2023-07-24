bbe9f17c8f141daa0c05ccd048f834ed1aafe5cf
Johnathan-Spicer <johnathanspicer335@gmail.com>
2023-07-24 14:57:56 +1200

    Python day 3 practice completed

--- main.py
+++ main.py
@@ -1,91 +1,128 @@
 import math
 import time
-# first_Name = 'Johnathan'
-# last_Name = 'Spicer'
-# full_Name = first_Name + " " + last_Name
-# print("Hello " + full_Name)
-# #print(type(name))
-# #print("Hello " + name)
+import random
+import os
+# # first_Name = 'Johnathan'
+# # last_Name = 'Spicer'
+# # full_Name = first_Name + " " + last_Name
+# # print("Hello " + full_Name)
+# # #print(type(name))
+# # #print("Hello " + name)
 
-# age = 16
-# age += 1
-# print(age)
-# print(type(age))
+# # age = 16
+# # age += 1
+# # print(age)
+# # print(type(age))
 
-# name = 'Johnathan'
-# age = 16
-# attractive = True
+# # name = 'Johnathan'
+# # age = 16
+# # attractive = True
 
-# name, age, attractive = 'Johnathan', 16, True
+# # name, age, attractive = 'Johnathan', 16, True
 
-# print(name, age, attractive)
-# len = length, find, capitalize, upper, lower, isdigit, isalpha, count, replace
+# # print(name, age, attractive)
+# # len = length, find, capitalize, upper, lower, isdigit, isalpha, count, replace
 
-# name = input("What is your name?: ")
-# age = int(input("How old are you?: "))
-# height = float(input("How tall are you?: "))
+# # name = input("What is your name?: ")
+# # age = int(input("How old are you?: "))
+# # height = float(input("How tall are you?: "))
 
-# print("Hello "+name + " " + "You are "+str(age)+" years old" + " " + "You are " +str(height)+"cm tall")
+# # print("Hello "+name + " " + "You are "+str(age)+" years old" + " " + "You are " +str(height)+"cm tall")
 
-#round = rounds the number to the nearest full int
-#math.ceil = rounds up to the nearest int so 3 would = to 4
-#math.floor = rounds down to the nearest int
-#abs = tells you how far away the number is from zero
-#pow = raise base number to a power so for example
-#3 to the power of 2 is 9
-#math.sqrt = short for square root
-#max finds the largest value
-#min finds the smallest value
+# #round = rounds the number to the nearest full int
+# #math.ceil = rounds up to the nearest int so 3 would = to 4
+# #math.floor = rounds down to the nearest int
+# #abs = tells you how far away the number is from zero
+# #pow = raise base number to a power so for example
+# #3 to the power of 2 is 9
+# #math.sqrt = short for square root
+# #max finds the largest value
+# #min finds the smallest value
 
-# name = ""
-# while len(name) == 0:
-#   name = input("Enter your name please: ")
-# print("Hello " + name)
+# # name = ""
+# # while len(name) == 0:
+# #   name = input("Enter your name please: ")
+# # print("Hello " + name)
 
-# #while loop = unlimited
-# #for loop = limited
+# # #while loop = unlimited
+# # #for loop = limited
 
-# # for i in range(10):
-# #   print(i+1)
+# # # for i in range(10):
+# # #   print(i+1)
 
-# for seconds in range(10,0,-1):
-#   print(seconds)
-#   time.sleep(1)
-# print("Happy New Year!")
+# # for seconds in range(10,0,-1):
+# #   print(seconds)
+# #   time.sleep(1)
+# # print("Happy New Year!")
 
-# rows = int(input("How many rows?: "))
-# columns = int(input("How many columns?: "))
-# symbol = input("Enter a symbol to use: ")
+# # rows = int(input("How many rows?: "))
+# # columns = int(input("How many columns?: "))
+# # symbol = input("Enter a symbol to use: ")
 
-# for i in range(rows):
-#   for j in range(columns):
-#     print(symbol, end="")
-#   print()
+# # for i in range(rows):
+# #   for j in range(columns):
+# #     print(symbol, end="")
+# #   print()
 
-# while True:
-#   name = input("Enter your name")
-#   if name != "":
-#     break
+# # while True:
+# #   name = input("Enter your name")
+# #   if name != "":
+# #     break
 
-# phone_number = "123-456-7890"
+# # phone_number = "123-456-7890"
 
-# for i in phone_number:
-#   if i == "-":
-#     continue
-#     print(i, end="")
+# # for i in phone_number:
+# #   if i == "-":
+# #     continue
+# #     print(i, end="")
 
-# for i in range(1, 21):
-#   if i == 13:
-#     pass
-#     else:
-#       print(i)
+# # for i in range(1, 21):
+# #   if i == 13:
+# #     pass
+# #     else:
+# #       print(i)
 
-# food = ["pizza", "burger", "sushi", "steak"]
-# # food[0] = "bagel"
-# # print(food[0])
-# # food.append("ice cream")
-# #food.pop()
+# # food = ["pizza", "burger", "sushi", "steak"]
+# # # food[0] = "bagel"
+# # # print(food[0])
+# # # food.append("ice cream")
+# # #food.pop()
 
-# for x in food:
-#   print(x)
+# # for x in food:
+# #   print(x)
+
+# def add(*args):
+#   sum = 0
+# args = list(args)
+# args[0] = 0
+#   for i in args:
+#     sum += i 
+#   return sum
+# print(add(1,2,3,4,5,6))  
+
+
+# ------------ kwargs ------------
+
+# def hello(**kwargs):
+#  # print("Hello " + kwargs['first'] + " " + kwargs['last'])
+#  print("Hello", end=" ")
+
+# for key,value in kwargs.items():
+#   print(value,end=" ")
+
+# hello(first="Johnathan",middle="James",last="Spicer")
+
+# animal = "cow"
+# item = "moon"
+# print("The {} jumped over the {}".format(animal, item))
+# print("The {0} jumped over the {1}".format(animal, item))
+# print("The {animal} jumped over the {item}".format(animal="cow", item="moon"))
+
+# x = random.randint(1,6)
+# y = random.random()
+# myList = ['rock', 'scissors', 'paper']
+# z = random.choice(myList)
+# cards = [1,2,3,4,5,6,7,8,9,"J","Q","K","A"]
+# random.shuffle(cards)
+# print(cards)
 
